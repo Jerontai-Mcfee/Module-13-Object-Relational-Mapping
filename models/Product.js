6a1750e6062d7df2acef065b54d6295245993af6
GuiltyGamingJay <jerontai07@gmail.com>
2023-02-03 16:40:47 -0500

    product.js  completed with notes

--- models/Product.js
+++ models/Product.js
@@ -4,19 +4,55 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
+// Create a new Sequelize model for product
+
 class Product extends Model {}
 
 // set up fields and rules for Product model
+// An `id` is automatically created by Sequelize, though best practice would be to define the primary key ourselves
+  // Define fields/columns on model
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    }
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
