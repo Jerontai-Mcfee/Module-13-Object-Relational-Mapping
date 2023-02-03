b08e5c176bdfaf083a372b92efb081291a6d76c8
GuiltyGamingJay <jerontai07@gmail.com>
2023-02-03 16:06:44 -0500

    category.js done with notes

--- models/Category.js
+++ models/Category.js
@@ -3,17 +3,31 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
-
+// Found in mini project models/ location
+ // Manually define the primary key
+  // Define fields/columns on model
+  // An `id` is automatically created by Sequelize, though best practice would be to define the primary key ourselves
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
+     // Prevent sequelize from renaming the table
+     // Set to false to remove `created_at` and `updated_at` fields
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
